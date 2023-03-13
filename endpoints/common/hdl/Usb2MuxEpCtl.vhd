9957f7d39fa11b34acba3de10f53f8b53b167b96
till straumann <till.straumann@alumni.tu-berlin.de>
2023-03-13 07:27:43 +0100

    Added Control-endpoint agent multiplexer

---
+++ endpoints/common/hdl/Usb2MuxEpCtl.vhd
@@ -0,0 +1,131 @@
+-- Copyright Till Straumann, 2023. Licensed under the EUPL-1.2 or later.
+-- You may obtain a copy of the license at
+--   https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+-- This notice must not be removed.
+
+-- Multiplexer to dispatch control requests directed to
+-- interfaces or endpoints.
+
+library ieee;
+use     ieee.std_logic_1164.all;
+use     ieee.numeric_std.all;
+
+use     work.Usb2UtilPkg.all;
+use     work.Usb2Pkg.all;
+use     work.Usb2MuxEpCtlPkg.all;
+
+entity Usb2MuxEpCtl is
+
+   generic (
+      AGENTS_G          : Usb2CtlEpAgentConfigArray
+   );
+   port (
+      usb2Clk           : in  std_logic;
+      usb2Rst           : in  std_logic;
+
+      -- connection to the Ctl endpoint
+      usb2CtlReqParamIb : in  Usb2CtlReqParamType;
+      usb2CtlExtOb      : out Usb2CtlExtType;
+      usb2CtlEpExtOb    : out Usb2EndpPairIbType;
+
+      -- connections to all the agents
+      -- do *NOT* connect any of the agents to usb2CtlReqParamIb !
+      -- The mux propagates 'vld' once it went through its arbitration
+      usb2CtlReqParamOb : out Usb2CtlReqParamType;
+      usb2CtlExtIb      : in  Usb2CtlExtArray( AGENTS_G'range )     := ( others => USB2_CTL_EXT_NAK_C );
+
+      usb2CtlEpExtIb    : in  Usb2EndpPairIbArray( AGENTS_G'range ) := ( others => USB2_ENDP_PAIR_IB_INIT_C )
+   );
+
+end entity Usb2MuxEpCtl;
+
+architecture Impl of Usb2MuxEpCtl is
+
+   type StateType is ( ARB, FWD );
+
+   subtype MuxIdxType is natural range AGENTS_G'range;
+
+   type RegType is record
+      state     : StateType;
+      sel       : MuxIdxType;
+   end record RegType;
+
+   constant REG_INIT_C : RegType := (
+      state     => ARB,
+      sel       => AGENTS_G'low
+   );
+
+   signal r     : RegType := REG_INIT_C;
+   signal rin   : RegType;
+
+   function accept(
+      constant p : in Usb2CtlReqParamType;
+      constant x : in Usb2CtlEpAgentConfigType
+   ) return boolean is
+   begin
+      if ( x.filtDir   and ( p.dev2Host  /= usb2ReqTypeIsDev2Host  ( x.reqType ) ) ) then
+         return false;
+      end if;
+      if ( x.filtType  and ( p.reqType   /= usb2ReqTypeGetType     ( x.reqType ) ) ) then
+         return false;
+      end if;
+      if ( x.filtRecpt and ( p.recipient /= usb2ReqTypeGetRecipient( x.reqType ) ) ) then
+         return false;
+      end if;
+      if ( p.index(7 downto 0) /= x.reqIndex ) then
+         return false;
+      end if ;
+      return true;
+   end function accept;
+
+begin
+
+   P_COMB : process ( r, usb2CtlReqParamIb, usb2CtlExtIb, usb2CtlEpExtIb ) is
+      variable v : RegType;
+   begin
+      v                 := r;
+
+      usb2CtlExtOb      <= USB2_CTL_EXT_NAK_C;
+      usb2CtlEpExtOb    <= usb2CtlEpExtIb( r.sel );
+      usb2CtlReqParamOb <= usb2CtlReqParamIb;
+
+      if ( usb2CtlReqParamIb.vld = '0' ) then
+         v.state := ARB;
+      end if;
+
+      case ( r.state ) is
+         when ARB =>
+            -- don't propagate 'vld' to the EPs until a choice is made
+            usb2CtlReqParamOb.vld <= '0';
+
+            if ( usb2CtlReqParamIb.vld = '1' ) then
+               L_ARB : for i in AGENTS_G'range loop
+                  if ( accept( usb2CtlReqParamIb, AGENTS_G(i) ) ) then
+                     v.state      := FWD;
+                     v.sel        := i;
+                     -- blank NAK during this cycle!
+                     usb2CtlExtOb <= USB2_CTL_EXT_INIT_C;
+                     exit L_ARB;
+                  end if;
+               end loop;
+            end if;
+
+         when FWD =>
+            usb2CtlExtOb <= usb2CtlExtIb( r.sel );
+      end case;
+
+      rin               <= v;
+   end process P_COMB;
+
+   P_SEQ : process ( usb2Clk ) is
+   begin
+      if ( rising_edge( usb2Clk ) ) then
+         if ( usb2Rst = '1' ) then
+            r <= REG_INIT_C;
+         else
+            r <= rin;
+         end if;
+      end if;
+   end process P_SEQ;
+
+end architecture Impl;
---
+++ endpoints/common/hdl/Usb2MuxEpCtlPkg.vhd
@@ -0,0 +1,177 @@
+-- Copyright Till Straumann, 2023. Licensed under the EUPL-1.2 or later.
+-- You may obtain a copy of the license at
+--   https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+-- This notice must not be removed.
+
+-- Package with definitions used by Usb2MuxEpCtl
+
+library ieee;
+use     ieee.std_logic_1164.all;
+use     ieee.numeric_std.all;
+
+use     work.Usb2UtilPkg.all;
+use     work.Usb2Pkg.all;
+
+package Usb2MuxEpCtlPkg is
+
+   -- describe what request type and index
+   -- identifies a particular agent.
+   type Usb2CtlEpAgentConfigType is record
+      -- filter based on the request type
+      -- can be selectively enabled
+      reqType  : Usb2ByteType;
+      filtDir  : boolean;
+      filtType : boolean;
+      filtRecpt: boolean;
+      reqIndex : Usb2ByteType;
+   end record Usb2CtlEpAgentConfigType;
+
+   function usb2CtlEpMkAgentConfig(
+      constant recipient : in std_logic_vector(1 downto 0);
+      constant index     : in natural range 0 to 255;
+      constant dev2Host  : in std_logic := 'X';
+      constant reqType   : in std_logic_vector(1 downto 0) := "XX"
+   ) return Usb2CtlEpAgentConfigType;
+
+   constant USB2_CTL_EP_AGENT_CONFIG_INIT_C : Usb2CtlEpAgentConfigType := (
+      reqType   => (others => '0'),   
+      filtDir   => false,
+      filtType  => false,
+      filtRecpt => false,
+      reqIndex  => (others => '0')
+   );
+
+   type Usb2CtlEpAgentConfigArray is array (natural range <>) of Usb2CtlEpAgentConfigType;
+
+   constant USB2_CTL_EP_AGENT_CONFIG_EMPTY_C : Usb2CtlEpAgentConfigArray(0 to -1) := (others => USB2_CTL_EP_AGENT_CONFIG_INIT_C);
+
+   function ite(
+      constant c : in boolean;
+      constant a : in Usb2CtlEpAgentConfigArray;
+      constant b : in Usb2CtlEpAgentConfigArray
+   ) return Usb2CtlEpAgentConfigArray;
+
+   function ite(
+      constant c : in boolean;
+      constant a : in Usb2CtlEpAgentConfigType;
+      constant b : in Usb2CtlEpAgentConfigArray := USB2_CTL_EP_AGENT_CONFIG_EMPTY_C
+   ) return Usb2CtlEpAgentConfigArray;
+
+
+   function usb2CtlEpMkIfcAgentConfig(
+      constant index     : in natural range 0 to 255;
+      constant dev2Host  : in std_logic := 'X';
+      constant reqType   : in std_logic_vector(1 downto 0) := "XX"
+   ) return Usb2CtlEpAgentConfigType;
+
+   function usb2CtlEpMkCsIfcAgentConfig(
+      constant index     : in natural range 0 to 255;
+      constant dev2Host  : in std_logic := 'X'
+   ) return Usb2CtlEpAgentConfigType;
+
+   function usb2CtlEpMkEpAgentConfig(
+      constant index     : in natural range 0 to 255;
+      constant dev2Host  : in std_logic := 'X';
+      constant reqType   : in std_logic_vector(1 downto 0) := "XX"
+   ) return Usb2CtlEpAgentConfigType;
+
+end package Usb2MuxEpCtlPkg;
+
+package body Usb2MuxEpCtlPkg is
+
+   function usb2CtlEpMkAgentConfig(
+      constant recipient : in std_logic_vector(1 downto 0);
+      constant index     : in natural range 0 to 255;
+      constant dev2Host  : in std_logic := 'X';
+      constant reqType   : in std_logic_vector(1 downto 0) := "XX"
+   ) return Usb2CtlEpAgentConfigType is
+      variable v   : Usb2CtlEpAgentConfigType;
+      variable d2h : boolean;
+      variable typ : std_logic_vector(1 downto 0) := "00";
+      variable rcp : std_logic_vector(1 downto 0) := "00";
+   begin
+      d2h         := (dev2Host   = '1');
+      v.filtDir   := (dev2Host  /= 'X');
+      v.filtType  := (reqType   /= "XX");
+      v.filtRecpt := (recipient /= "XX");
+
+      if ( v.filtType ) then
+         typ := reqType;
+      end if;
+
+      if ( v.filtRecpt ) then
+         rcp := recipient;
+      end if;
+
+      v.reqType  := usb2MakeRequestType( d2h, typ, rcp );
+      v.reqIndex := std_logic_vector( to_unsigned( index, 8 ) );
+
+      return v;
+
+   end function usb2CtlEpMkAgentConfig;
+
+   function usb2CtlEpMkIfcAgentConfig(
+      constant index     : in natural range 0 to 255;
+      constant dev2Host  : in std_logic := 'X';
+      constant reqType   : in std_logic_vector(1 downto 0) := "XX"
+   ) return Usb2CtlEpAgentConfigType is
+   begin
+      return usb2CtlEpMkAgentConfig (
+         recipient => USB2_REQ_TYP_RECIPIENT_IFC_C,
+         index     => index,
+         dev2Host  => dev2Host,
+         reqType   => reqType
+      );
+   end function usb2CtlEpMkIfcAgentConfig;
+
+   function usb2CtlEpMkEpAgentConfig(
+      constant index     : in natural range 0 to 255;
+      constant dev2Host  : in std_logic := 'X';
+      constant reqType   : in std_logic_vector(1 downto 0) := "XX"
+   ) return Usb2CtlEpAgentConfigType is
+   begin
+      return usb2CtlEpMkAgentConfig (
+         recipient => USB2_REQ_TYP_RECIPIENT_EPT_C,
+         index     => index,
+         dev2Host  => dev2Host,
+         reqType   => reqType
+      );
+   end function usb2CtlEpMkEpAgentConfig;
+
+   function usb2CtlEpMkCsIfcAgentConfig(
+      constant index     : in natural range 0 to 255;
+      constant dev2Host  : in std_logic := 'X'
+   ) return Usb2CtlEpAgentConfigType is
+   begin
+      return usb2CtlEpMkIfcAgentConfig (
+         index     => index,
+         dev2Host  => dev2Host,
+         reqType   => USB2_REQ_TYP_TYPE_CLASS_C
+      );
+   end function usb2CtlEpMkCsIfcAgentConfig;
+
+   function ite(
+      constant c : in boolean;
+      constant a : in Usb2CtlEpAgentConfigArray;
+      constant b : in Usb2CtlEpAgentConfigArray
+   ) return Usb2CtlEpAgentConfigArray is
+   begin
+      if ( c ) then return a; else return b; end if;
+   end function ite;
+
+   function ite(
+      constant c : in boolean;
+      constant a : in Usb2CtlEpAgentConfigType;
+      constant b : in Usb2CtlEpAgentConfigArray := USB2_CTL_EP_AGENT_CONFIG_EMPTY_C
+   ) return Usb2CtlEpAgentConfigArray is
+      variable v : Usb2CtlEpAgentConfigArray(0 to 0);
+   begin
+      if ( c ) then
+         v(0) := a;
+         return v;
+      else
+         return b;
+      end if;
+   end function ite;
+
+end package body Usb2MuxEpCtlPkg;
